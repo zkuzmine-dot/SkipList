f2524b379328e3c8f5587de4896ce8f1d97680d2
zkuzmine-dot <zenifig@gmail.com>
2025-04-25 20:06:33 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary3/Class1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class SkipList<TKey, TValue> where TKey : IComparable<TKey>
+{
+    private class Node<TNodeKey, TNodeValue>
+    {
+        public TNodeKey Key { get; }
+        public TNodeValue Value { get; set; }
+        public Node<TNodeKey, TNodeValue>[] Forward { get; }
+
+        public Node(TNodeKey key, TNodeValue value, int level)
+        {
+            Key = key;
+            Value = value;
+            Forward = new Node<TNodeKey, TNodeValue>[level + 1];
+        }
+    }
+
+    private const int MaxLevel = 32;
+    private readonly Random _random = new Random();
+    private int _currentLevel;
+    private Node<TKey, TValue> _header;
+
+    public SkipList()
+    {
+        _header = new Node<TKey, TValue>(default, default, MaxLevel);
+        _currentLevel = 0;
+    }
+
+    public void Add(TKey key, TValue value)
+    {
+        var update = new Node<TKey, TValue>[MaxLevel + 1];
+        var current = _header;
+
+        for (int i = _currentLevel; i >= 0; i--)
+        {
+            while (current.Forward[i] != null && current.Forward[i].Key.CompareTo(key) < 0)
+                current = current.Forward[i];
+            update[i] = current;
+        }
+
+        current = current.Forward[0];
+        if (current != null && current.Key.CompareTo(key) == 0)
+        {
+            current.Value = value;
+            return;
+        }
+
+        int level = RandomLevel();
+        if (level > _currentLevel)
+        {
+            for (int i = _currentLevel + 1; i <= level; i++)
+                update[i] = _header;
+            _currentLevel = level;
+        }
+
+        var newNode = new Node<TKey, TValue>(key, value, level);
+        for (int i = 0; i <= level; i++)
+        {
+            newNode.Forward[i] = update[i].Forward[i];
+            update[i].Forward[i] = newNode;
+        }
+    }
+
+    public bool Remove(TKey key)
+    {
+        var update = new Node<TKey, TValue>[MaxLevel + 1];
+        var current = _header;
+
+        for (int i = _currentLevel; i >= 0; i--)
+        {
+            while (current.Forward[i] != null && current.Forward[i].Key.CompareTo(key) < 0)
+                current = current.Forward[i];
+            update[i] = current;
+        }
+
+        current = current.Forward[0];
+        if (current == null || current.Key.CompareTo(key) != 0)
+            return false;
+
+        for (int i = 0; i <= _currentLevel; i++)
+        {
+            if (update[i].Forward[i] != current)
+                break;
+            update[i].Forward[i] = current.Forward[i];
+        }
+
+        while (_currentLevel > 0 && _header.Forward[_currentLevel] == null)
+            _currentLevel--;
+
+        return true;
+    }
+
+    public bool TryGetValue(TKey key, out TValue value)
+    {
+        var current = _header;
+
+        for (int i = _currentLevel; i >= 0; i--)
+        {
+            while (current.Forward[i] != null && current.Forward[i].Key.CompareTo(key) < 0)
+                current = current.Forward[i];
+        }
+
+        current = current.Forward[0];
+        if (current != null && current.Key.CompareTo(key) == 0)
+        {
+            value = current.Value;
+            return true;
+        }
+
+        value = default;
+        return false;
+    }
+
+    private int RandomLevel()
+    {
+        int level = 0;
+        while (_random.NextDouble() < 0.5 && level < MaxLevel)
+            level++;
+        return level;
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 10000;//
+        int[] array = GenerateUniqueNumbers(n).ToArray();
+
+        // Тестирование SkipList
+        var skipList = new SkipList<int, int>();
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        foreach (var num in array)
+            skipList.Add(num, num);
+        stopwatch.Stop();
+        Console.WriteLine($"SkipList заполнен за {stopwatch.ElapsedMilliseconds} мс");
+
+        // Удаление элементов от n/2 до 3n/4
+        stopwatch.Restart();
+        for (int i = n / 2; i < 3 * n / 4; i++)
+            skipList.Remove(array[i]);
+        stopwatch.Stop();
+        Console.WriteLine($"SkipList удаление: {stopwatch.ElapsedMilliseconds} мс");
+
+        // Поиск всех элементов
+        stopwatch.Restart();
+        foreach (var num in array)
+            skipList.TryGetValue(num, out _);
+        stopwatch.Stop();
+        Console.WriteLine($"SkipList поиск: {stopwatch.ElapsedMilliseconds} мс");
+
+        // Тестирование SortedList
+        var sortedList = new SortedList<int, int>();
+        stopwatch.Restart();
+        foreach (var num in array)
+            sortedList.Add(num, num);
+        stopwatch.Stop();
+        Console.WriteLine($"SortedList заполнен за {stopwatch.ElapsedMilliseconds} мс");
+
+        stopwatch.Restart();
+        for (int i = n / 2; i < 3 * n / 4; i++)
+            sortedList.Remove(array[i]);
+        stopwatch.Stop();
+        Console.WriteLine($"SortedList удаление: {stopwatch.ElapsedMilliseconds} мс");
+
+        stopwatch.Restart();
+        foreach (var num in array)
+            sortedList.TryGetValue(num, out _);
+        stopwatch.Stop();
+        Console.WriteLine($"SortedList поиск: {stopwatch.ElapsedMilliseconds} мс");
+    }
+
+    private static HashSet<int> GenerateUniqueNumbers(int count)
+    {
+        var random = new Random();
+        var numbers = new HashSet<int>();
+        while (numbers.Count < count)
+            numbers.Add(random.Next());
+        return numbers;
+    }
+}
